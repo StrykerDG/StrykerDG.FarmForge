c71018503254c66795b9992dc9819b433a4ce2bc
StrykerDG <stryker@strykerdg.com>
2020-09-28 18:29:51 -0500

    Api Endpoints
    Added Actor methods and Controller endpoints for user accounts

--- StrykerDG.FarmForge.Api/Controllers/AuthenticationController.cs
+++ StrykerDG.FarmForge.Api/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 ﻿using Akka.Actor;
 using Akka.Util;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StrykerDG.FarmForge.Actors;
 using StrykerDG.FarmForge.Actors.Authentication.Messages;
@@ -33,6 +34,35 @@ namespace StrykerDG.FarmForge.LocalApi.Controllers
 
             var result = await AuthActor.Ask(new AskToLogin(login.Username, login.Password));
 
+            return ValidateActorResult(result);
+        }
+
+        [HttpGet("Users")]
+        [Authorize(Policy = "AuthenticatedWebClient")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var result = await AuthActor.Ask(new AskForUsers());
+            return Ok(result);
+        }
+
+        [HttpPost("Users")]
+        [Authorize(Policy = "AuthenticatedWebClient")]
+        public async Task<IActionResult> CreateUser([FromBody]LoginDTO user)
+        {
+            var result = await AuthActor.Ask(new AskToCreateUser(user.Username, user.Password));
+            return ValidateActorResult(result);
+        }
+
+        [HttpDelete("Users/{userId}")]
+        [Authorize(Policy = "AuthenticatedWebClient")]
+        public async Task<IActionResult> DeleteUser(int userId)
+        {
+            var requestor = HttpContext.User.Claims
+                .Where(c => c.Type == "User")
+                .FirstOrDefault()
+                ?.Value;
+
+            var result = await AuthActor.Ask(new AskToDeleteUser(userId, requestor));
             return ValidateActorResult(result);
         }
     }
