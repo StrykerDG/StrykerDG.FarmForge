161ed73a9d62290c9d1f025fb3994126864fa26d
StrykerDG <stryker@strykerdg.com>
2020-10-29 04:42:59 -0500

    Cleanup
    Removing commented out code

--- StrykerDG.FarmForge.Actors/Products/Messages/AskToCreateProductCategory.cs
+++ StrykerDG.FarmForge.Actors/Products/Messages/AskToCreateProductCategory.cs
@@ -7,14 +7,10 @@ namespace StrykerDG.FarmForge.Actors.Products.Messages
 {
     public class AskToCreateProductCategory
     {
-        //public string Label { get; private set; }
-        //public string Description { get; private set; }
         public ProductCategory ProductCategory { get; private set; }
 
         public AskToCreateProductCategory(ProductCategory category)
         {
-            //Label = label;
-            //Description = description;
             ProductCategory = category;
         }
     }
