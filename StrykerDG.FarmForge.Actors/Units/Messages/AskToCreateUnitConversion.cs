8b3e87e6207b31b76324e0c9b7d75d1d5f063d4a
StrykerDG <stryker@strykerdg.com>
2020-10-29 06:53:17 -0500

    Unit Endpoints and Actor Updates
    Added enpdoints for creating, updating, deleting unit types and conversions. Added logic for creating and deleting unit types and conversions

---
+++ StrykerDG.FarmForge.Actors/Units/Messages/AskToCreateUnitConversion.cs
@@ -0,0 +1,17 @@
+﻿using StrykerDG.FarmForge.DataModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrykerDG.FarmForge.Actors.Units.Messages
+{
+    public class AskToCreateUnitConversion
+    {
+        public UnitTypeConversion Conversion { get; private set; }
+
+        public AskToCreateUnitConversion(UnitTypeConversion conversion)
+        {
+            Conversion = conversion;
+        }
+    }
+}
---
+++ StrykerDG.FarmForge.Actors/Units/Messages/AskToDeleteUnitType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrykerDG.FarmForge.Actors.Units.Messages
+{
+    public class AskToDeleteUnitType
+    {
+        public int UnitTypeId { get; private set; }
+
+        public AskToDeleteUnitType(int id)
+        {
+            UnitTypeId = id;
+        }
+    }
+}
